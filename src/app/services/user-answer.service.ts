0f9aab6fbe5087736d22499653d06744debf3c9a
emmah <emmahenault31@gmail.com>
2025-04-05 19:24:54 +0200

    fix/userAnswerService

--- src/app/services/user-answer.service.ts
+++ src/app/services/user-answer.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserAnswer } from '../models/user-answer';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class UserAnswerService {
-  private apiUrl = 'http://localhost:8080/user-answers';
+  private apiUrl = environment.apiUrl + 'user-answers';
 
   constructor(private http: HttpClient) {}
 
-  // Envoie d'une réponse utilisateur
   addUserAnswer(answer: UserAnswer): Observable<UserAnswer> {
     return this.http.post<UserAnswer>(this.apiUrl, answer);
   }
