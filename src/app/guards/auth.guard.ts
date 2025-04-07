7d7a020078af4d7aff14df86df4815eb097f6456
emmah <emmahenault31@gmail.com>
2025-04-07 10:33:25 +0200

    feat/guard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,20 @@
+// src/app/guards/auth.guard.ts
+import { Injectable } from '@angular/core';
+import { CanActivateChild, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service'; // ton service d'authentification
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivateChild {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivateChild(): boolean | Observable<boolean> | Promise<boolean> {
+    if (this.authService.isAuthenticated()) {
+      return true;
+    }
+    this.router.navigate(['/']);
+    return false;
+  }
+}
